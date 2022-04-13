689f5f15aeccb1ba13a1241051c7f1f5e70f4dfe
Ivan1F <wyf0762@foxmail.com>
2022-04-13 08:44:49 +0800

    list plugins command

--- mcdreforged_plugin_manager/entries/mcdr_entry.py
+++ mcdreforged_plugin_manager/entries/mcdr_entry.py
@@ -1,6 +1,21 @@
 from mcdreforged.api.all import *
 
+from mcdreforged_plugin_manager.commands import CommandExecutor
+from mcdreforged_plugin_manager.config import config
+
+command_executor = CommandExecutor()
+
 
 def on_load(server: PluginServerInterface, old):
-    from mcdreforged_plugin_manager.util.cache import cache
-    pass
+    def get_literal(literal: str):
+        return Literal(literal).requires(lambda src, ctx: src.has_permission(config.permission))
+    server.register_command(
+        Literal('!!mpm')
+        .then(
+            get_literal('list')
+            .then(
+                Text('labels')
+                .runs(lambda src, ctx: command_executor.with_source(src).list_plugins(ctx['labels'].split(',')))
+            )
+        )
+    )
