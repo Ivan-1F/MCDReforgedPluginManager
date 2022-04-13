ff36d0c33d2c9fc35ccfa1fd29374eb95b52cc7a
Ivan1F <wyf0762@foxmail.com>
2022-04-13 13:04:32 +0800

    markdown_util -> text_util extract storage @ cache to storage.py

--- mcdreforged_plugin_manager/util/cache.py
+++ mcdreforged_plugin_manager/util/cache.py
@@ -1,88 +1,14 @@
 import json
 import os
 import traceback
-from typing import List, Dict, Optional, Union
 
 import requests
 from mcdreforged.api.all import *
 
 from mcdreforged_plugin_manager.config import config
 from mcdreforged_plugin_manager.constants import psi
-from mcdreforged_plugin_manager.util.markdown_util import parse_markdown, italic, link, new_line, command_run
+from mcdreforged_plugin_manager.util.storage import PluginMetaInfoStorage
 from mcdreforged_plugin_manager.util.translation import tr
-
-
-class MetaInfo(Serializable):
-    id: str
-    name: str
-    version: str
-    repository: str
-    labels: List[str]
-    authors: List[str]
-    dependencies: Dict[str, str]
-    requirements: List[str]
-    description: Dict[str, str]
-
-    @property
-    def formatted_description(self) -> RTextBase:
-        if not self.description:
-            return italic(tr('plugin.no_description'))
-        language = psi.get_mcdr_language()
-        text = self.description.get(language)
-        if text is None:
-            text = list(self.description.values())[0]
-        return parse_markdown(text)
-
-    @property
-    def version_text(self):
-        return RText('({}@{})'.format(self.id, self.version), RColor.gray)
-
-    @property
-    def action_bar(self):
-        return RTextList(
-            command_run('[↓]', '!!mpm install {}'.format(self.id), tr('plugin.operation.install')).set_color(RColor.green),
-            command_run('[ⓘ]', '!!mpm info {}'.format(self.id), tr('plugin.operation.show_info')).set_color(RColor.aqua)
-        )
-
-    @property
-    def brief(self):
-        return RTextList(
-            self.action_bar,
-            '\n',
-            RTextList(
-                '- ',
-                link(RText(self.name), self.repository),
-                ' ',
-                self.version_text
-            ),
-            new_line(),
-            RTextList(
-                tr('plugin.author', ', '.join(self.authors)).set_color(RColor.aqua),
-                ' | ',
-                tr('plugin.label', ', '.join(self.labels)).set_color(RColor.aqua),
-            ),
-            new_line(),
-            self.formatted_description
-        )
-
-
-class PluginMetaInfoStorage(Serializable):
-    plugin_amount: int = 0
-    plugins: Dict[str, MetaInfo] = {}  # plugin id -> plugin meta
-
-    def get_plugins_by_labels(self, labels: Optional[Union[type(None), str, List[str]]] = None):
-        if labels is None:
-            labels = ['information', 'tool', 'management', 'api']
-        if isinstance(labels, str):
-            labels = [labels]
-        for plugin in self.plugins.values():
-            if any([label in labels for label in plugin.labels]):
-                yield plugin
-
-    def search(self, query: str):
-        for plugin in self.plugins.values():
-            if query in plugin.name or query in plugin.id:
-                yield plugin
 
 
 class Cache(PluginMetaInfoStorage):
---
+++ mcdreforged_plugin_manager/util/storage.py
@@ -0,0 +1,80 @@
+from typing import List, Dict, Optional, Union
+
+from mcdreforged.api.all import *
+
+from mcdreforged_plugin_manager.constants import psi
+from mcdreforged_plugin_manager.util.text_util import italic, parse_markdown, command_run, link, new_line
+from mcdreforged_plugin_manager.util.translation import tr
+
+
+class MetaInfo(Serializable):
+    id: str
+    name: str
+    version: str
+    repository: str
+    labels: List[str]
+    authors: List[str]
+    dependencies: Dict[str, str]
+    requirements: List[str]
+    description: Dict[str, str]
+
+    @property
+    def formatted_description(self) -> RTextBase:
+        if not self.description:
+            return italic(tr('plugin.no_description'))
+        language = psi.get_mcdr_language()
+        text = self.description.get(language)
+        if text is None:
+            text = list(self.description.values())[0]
+        return parse_markdown(text)
+
+    @property
+    def version_text(self):
+        return RText('({}@{})'.format(self.id, self.version), RColor.gray)
+
+    @property
+    def action_bar(self):
+        return RTextList(
+            command_run('[↓]', '!!mpm install {}'.format(self.id), tr('plugin.operation.install')).set_color(RColor.green),
+            command_run('[ⓘ]', '!!mpm info {}'.format(self.id), tr('plugin.operation.show_info')).set_color(RColor.aqua)
+        )
+
+    @property
+    def brief(self):
+        return RTextList(
+            self.action_bar,
+            '\n',
+            RTextList(
+                '- ',
+                link(RText(self.name), self.repository),
+                ' ',
+                self.version_text
+            ),
+            new_line(),
+            RTextList(
+                tr('plugin.author', ', '.join(self.authors)).set_color(RColor.aqua),
+                ' | ',
+                tr('plugin.label', ', '.join(self.labels)).set_color(RColor.aqua),
+            ),
+            new_line(),
+            self.formatted_description
+        )
+
+
+class PluginMetaInfoStorage(Serializable):
+    plugin_amount: int = 0
+    plugins: Dict[str, MetaInfo] = {}  # plugin id -> plugin meta
+
+    def get_plugins_by_labels(self, labels: Optional[Union[type(None), str, List[str]]] = None):
+        if labels is None:
+            labels = ['information', 'tool', 'management', 'api']
+        if isinstance(labels, str):
+            labels = [labels]
+        for plugin in self.plugins.values():
+            if any([label in labels for label in plugin.labels]):
+                yield plugin
+
+    def search(self, query: str):
+        for plugin in self.plugins.values():
+            if query in plugin.name or query in plugin.id:
+                yield plugin
