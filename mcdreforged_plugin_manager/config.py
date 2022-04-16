fc74b2634f777bbe8b43e51ec28a3deddf48126d
Ivan1F <wyf0762@foxmail.com>
2022-04-16 19:37:28 +0800

    commented default config

--- mcdreforged_plugin_manager/config.py
+++ mcdreforged_plugin_manager/config.py
@@ -1,13 +1,15 @@
 import os
 
 from mcdreforged.api.all import *
-from ruamel.yaml import YAML
+from ruamel.yaml import YAML, CommentedMap
 
 from mcdreforged_plugin_manager.constants import psi
+from mcdreforged_plugin_manager.util.translation import tr
 
 
 class Configure(Serializable):
     CONFIG_PATH = os.path.join(psi.get_data_folder(), 'config.yml')
+    DEFAULT_CONFIG = psi.open_bundled_file('resources/default_config.yml')
 
     permission: int = PermissionLevel.PHYSICAL_SERVER_CONTROL_LEVEL
     source: str = 'https://raw.githubusercontent.com/MCDReforged/PluginCatalogue/meta'
@@ -22,18 +24,20 @@ class Configure(Serializable):
     @classmethod
     def load(cls) -> 'Configure':
         if not os.path.isfile(cls.CONFIG_PATH):
+            cls.__save_default()
             default = cls.get_default()
-            default.save()
             return default
         with open(cls.CONFIG_PATH, 'r', encoding='UTF-8') as f:
             data = YAML().load(f)
         return cls.deserialize(data)
 
-    def save(self):
-        if not os.path.isdir(os.path.dirname(self.CONFIG_PATH)):
-            os.makedirs(os.path.dirname(self.CONFIG_PATH))
-        with open(self.CONFIG_PATH, 'w+', encoding='UTF-8') as f:
-            YAML().dump(self.serialize(), f)
+    @classmethod
+    def __save_default(cls):
+        if not os.path.isdir(os.path.dirname(cls.CONFIG_PATH)):
+            os.makedirs(os.path.dirname(cls.CONFIG_PATH))
+        with open(cls.CONFIG_PATH, 'w', encoding='utf8') as file:
+            data: CommentedMap = YAML().load(cls.DEFAULT_CONFIG)
+            YAML().dump(data, file)
 
 
 config = Configure.load()
